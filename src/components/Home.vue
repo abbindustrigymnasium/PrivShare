4acf39f574ee1fbf8ff1ea1b661bf8f1dd371921
ABBNojGau <116065858+ABBNojGau@users.noreply.github.com>
2025-05-07 23:52:05 +0200

    added centering

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="home">
-    <h1>Welcome</h1>
+  <div class="home-grid">
+    <h1
+      style="
+        grid-column: 1 / -1;
+        text-align: center;
+        font-size: 2em;
+        margin-bottom: 20px;
+        color: white;
+      "
+    >
+      Welcome to PrivShare, the private file sharing solution without any
+      central server!
+    </h1>
     <div class="button-group">
-      <router-link to="/download"><button>Download</button></router-link>
-      <router-link to="/upload"><button>Upload</button></router-link>
+      <router-link to="/download">
+        <button>Download</button>
+      </router-link>
+      <router-link to="/upload">
+        <button>Upload</button>
+      </router-link>
     </div>
   </div>
 </template>
